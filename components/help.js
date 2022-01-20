27e781093c2671a4182a5860a51ff861f3dbb5a4
Matej Lednicky <matej.lednicky@typeform.com>
2022-01-20 17:14:34 +0100

    update help

--- components/help.js
+++ components/help.js
@@ -22,17 +22,70 @@ export const Help = ({ close }) => (
         </li>
         <li>
           <span style={highlightStyles[1]}>oranžová</span> - písmeno sa v
-          hádanom slove nachádza, ale na inej pozícii ako v tvojom slove
+          hádanom slove nachádza, ale na inej pozícii ako v tvojom slove (môže
+          byť na už uhádnutej pozícii)
         </li>
         <li>
           <span style={highlightStyles[2]}>zelená</span> - písmeno sa v hádanom
-          slove nachádza na tom istom mieste ako v tvojom slove
+          slove nachádza na tom istom mieste ako v tvojom slove (ale môže sa
+          nachádzať aj viac krát - na iných miestach)
+        </li>
+      </ul>
+      <p>
+        Napríklad ak hádaš slovo <span className={styles.message}>ROBOT</span> a
+        zadáš:
+      </p>
+      <ul>
+        <li>
+          <span style={highlightStyles[0]}>V</span>
+          <span style={highlightStyles[0]}>Ý</span>
+          <span style={highlightStyles[1]}>R</span>
+          <span style={highlightStyles[0]}>U</span>
+          <span style={highlightStyles[1]}>B</span>
+          <ul>
+            <li>R a B sa v hľadanom slove nachádzajú, ale na iných miestach</li>
+            <li>V, Ý a U sa v hľadanom slove nenachádzajú</li>
+          </ul>
+        </li>
+        <li>
+          <span style={highlightStyles[0]}>H</span>
+          <span style={highlightStyles[0]}>U</span>
+          <span style={highlightStyles[0]}>M</span>
+          <span style={highlightStyles[2]}>O</span>
+          <span style={highlightStyles[1]}>R</span>
+          <ul>
+            <li>O sa v hľadanom slove nachádza, tiež na 4. mieste</li>
+            <li>R sa v hľadanom slove nachádza, ale na 1. mieste, nie na 5.</li>
+            <li>H, U a M sa v hľadanom slove nenachádzajú</li>
+          </ul>
+        </li>
+        <li>
+          <span style={highlightStyles[1]}>B</span>
+          <span style={highlightStyles[2]}>O</span>
+          <span style={highlightStyles[2]}>B</span>
+          <span style={highlightStyles[2]}>O</span>
+          <span style={highlightStyles[1]}>R</span>
+          <ul>
+            <li>
+              O, B a O sa v hľadanom slove nachádzajú - na 2., 3., a 4. mieste
+            </li>
+            <li>B sa v hľadanom slove nachádza na inom mieste (na 3.)</li>
+            <li>R sa v hľadanom slove nachádza na inom mieste (na 1.)</li>
+          </ul>
         </li>
       </ul>
       <p>
         Inšpirované hrou{" "}
         <a href="https://www.powerlanguage.co.uk/wordle/" target="_blank">
           Wordle
+        </a>
+        . Naprogramované v{" "}
+        <a href="https://nextjs.org/" target="_blank">
+          NextJS
+        </a>
+        , zverejnené{" "}
+        <a href="https://github.com/mathio/hadaj-slovo" target="_blank">
+          na Githube
         </a>
         .
       </p>
